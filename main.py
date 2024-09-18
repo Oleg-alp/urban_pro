96e270c595b189caec65d65465b98a54f6d52703
oleg_alp <skalolaz6500@mail.ru>
2024-09-18 22:17:16 +0300

    отправление файла

---
+++ main.py
@@ -0,0 +1,26 @@
+#1st program
+print((9**0.5)*5)
+print(12+89-1)
+#2nd program
+print(9.99>9.8 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+#4th program
+print(type(float('123.456')))
+print(123.456*10)
+example='Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+home_work = 'всего задач:12,'
+time_spent = 'затраченных часов:1.5,'
+name_course = 'Курс: Python,'
+average_time ='среднее время выполнения',1.5/12, 'часа'
+print(name_course, home_work, time_spent, average_time)
+
+
+
+
