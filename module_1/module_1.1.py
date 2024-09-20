15c4af678984b9c1b53c1dde2766c8d8996ff80d
oleg_alp <skalolaz6500@mail.ru>
2024-09-20 21:41:59 +0300

    отправление файла

---
+++ module_1/module_1.1.py
@@ -0,0 +1,5 @@
+home_work = 12
+time_spent = 1.5
+name_course = 'Python'
+average_time = 1.5/12
+print('Курс:',name_course,'всего задач:',home_work,'затрачено часов:',time_spent,'среднее время выполнения:',average_time,'часов:')
